6daa6af4745a83cd690eb0dffdb71f5cd4ca3399
mazen <mazen15408@gmail.com>
2024-11-18 16:35:59 +0200

    fold and shift done

--- gui.py
+++ gui.py
@@ -3,7 +3,8 @@ from signal_generation import generate_signal, plot_signal_from_file
 from adder_subtracter import load_and_process_files
 from quantization import quantize_and_save_signal
 from fourier import dft, idft
-
+from shiftAndFold import sfrun
+from DerivativeSignal import DerivativeSignal
 # Function to create signal generation interface
 def task_one_interface():
     root = tk.Tk()
@@ -74,4 +75,6 @@ def home_page(root):
     tk.Button(root, text="quantization", bg="#E7DECC", fg="#1569C7", font=("Helvetica", 12), command=lambda: quantize_and_save_signal(root)).pack(pady=15)
     tk.Button(root, text="DFT", bg="#E7DECC", fg="#1569C7", font=("Helvetica", 12), command=lambda: dft(root)).pack(pady=15)
     tk.Button(root, text="IDFT", bg="#E7DECC", fg="#1569C7", font=("Helvetica", 12), command=lambda: idft(root)).pack(pady=15)
+    tk.Button(root, text="Shift and Fold", bg="#E7DECC", fg="#1569C7", font=("Helvetica", 12), command=lambda: sfrun(root)).pack(pady=15)
+    tk.Button(root, text="DerivativeSignal", bg="#E7DECC", fg="#1569C7", font=("Helvetica", 12), command=lambda: DerivativeSignal(root)).pack(pady=15)
    
